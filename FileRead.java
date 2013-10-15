bab0b9e8a49b3e9a7125a2cca0e02def00dc4f20
sasiofs <sasikumar_svks@yahoo.co.in>
2013-10-15 13:14:42 +0530

    Update FileRead.java

--- FileRead.java
+++ FileRead.java
@@ -1,4 +1,3 @@
-
 public class FileRead {
 	public static void main(String[] args)  
     {  
@@ -8,6 +7,7 @@ public class FileRead {
         @SuppressWarnings("unused")
      
 		Process process = runtime.exec("C:/WINDOWS/system32/notepad.exe D:/Java/FIO/FIOTest.txt");  
+		System.out.println("Sasi");
       }  
       catch (Exception e)  
       {  
