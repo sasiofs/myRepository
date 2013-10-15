422cd0941c9b3b786aa90db1592285e801ee56b2
sasikumarr <sasikumarr@PC1222.itp.objectfrontier.com>
2013-10-15 13:13:53 +0530

    Upload One file...

---
+++ FileRead.java
@@ -0,0 +1,17 @@
+
+public class FileRead {
+	public static void main(String[] args)  
+    {  
+      try   
+      {  
+        Runtime runtime = Runtime.getRuntime();  
+        @SuppressWarnings("unused")
+     
+		Process process = runtime.exec("C:/WINDOWS/system32/notepad.exe D:/Java/FIO/FIOTest.txt");  
+      }  
+      catch (Exception e)  
+      {  
+        System.out.println("error is:"+e);  
+      } 
+    }  
+}
